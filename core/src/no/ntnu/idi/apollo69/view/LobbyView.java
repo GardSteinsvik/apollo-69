f22a0b86709bea1518af016763c3298becc2d32d
Anders Pedersen <andped@stud.ntnu>
2020-03-12 19:18:24 +0100

    Lobby gui "finished". No functionality made.

--- core/src/no/ntnu/idi/apollo69/view/LobbyView.java
+++ core/src/no/ntnu/idi/apollo69/view/LobbyView.java
@@ -1,41 +1,48 @@
 package no.ntnu.idi.apollo69.view;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.ui.Value;
+import com.badlogic.gdx.scenes.scene2d.utils.BaseDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-
+import no.ntnu.idi.apollo69.Variables;
 import no.ntnu.idi.apollo69.controller.LobbyController;
 import no.ntnu.idi.apollo69.model.LobbyModel;
 
-public class LobbyView extends ApplicationAdapter implements Screen {
-
-    final static float SIZEBUTTONX = 3f;
-    final static float SIZEBUTTONY = 3f;
+public class LobbyView extends ApplicationAdapter implements Screen, Variables {
 
     private SpriteBatch spriteBatch;
     private  LobbyController lobbyController;
     private LobbyModel lobbyModel;
-
     private Stage stage;
-
     private Texture backgroundTexture;
-
 
     public LobbyView(LobbyController lobbyController, LobbyModel lobbyModel, SpriteBatch spriteBatch) {
         this.lobbyController=lobbyController;
@@ -43,60 +50,94 @@ public class LobbyView extends ApplicationAdapter implements Screen {
         this.spriteBatch = spriteBatch;
 
         stage = new Stage(new ScreenViewport());
+
         Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void show() {
+
         backgroundTexture = new Texture(Gdx.files.internal("background.png"));
-
-        Group group = new Group();
-
-        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+        HorizontalGroup group = new HorizontalGroup();
+        Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
 
         float sizeWidth = Gdx.graphics.getWidth();
         float sizeHeight = Gdx.graphics.getHeight();
 
-        group.setSize(sizeWidth, sizeHeight);
+        skin.getFont("font-label").getData().setScale(Gdx.graphics.getDensity());
+
         Table tableLeft = new Table(skin);
         tableLeft.setFillParent(false);
-        tableLeft.setHeight(sizeHeight);
-        tableLeft.setWidth(sizeWidth*0.5f);
+        tableLeft.setHeight(sizeHeight*1.5f);
+        tableLeft.setWidth(sizeWidth);
+
         Table tableRight = new Table(skin);
-        // TODO: If text field is ugly, make own style.
-//        TextField.TextFieldStyle style = new TextField.TextFieldStyle();
-        TextField nickname = new TextField("nickname", skin);
+        tableRight.setFillParent(false);
+        tableRight.align(Align.right);
+        tableRight.setHeight(sizeHeight*1.5f);
+        tableRight.setWidth(sizeWidth);
 
-        TextButton join = new TextButton("Join",skin);
-        join.getLabel().setFontScale(SIZEBUTTONX, SIZEBUTTONY);
+        List highScoreList = new List(skin);
+        highScoreList.setFillParent(true);
+        highScoreList.setItems(getFakeHighScoreList());
 
-        TextButton exit = new TextButton("Exit lobby", skin);
-        exit.getLabel().setFontScale(SIZEBUTTONX, SIZEBUTTONX);
+        TextField nickname = new TextField(textNickname, skin);
 
-        tableLeft.align(Align.left);
+        TextButton join = new TextButton(textJoin,skin);
+        join.getLabel().setFontScale(sizeButton);
+
+        TextButton exit = new TextButton(textExit, skin);
+        exit.getLabel().setFontScale(sizeButton);
+
         tableLeft.setWidth(sizeWidth);
         tableLeft.setHeight(sizeHeight);
 
-        tableLeft.row().colspan(3).fillX().pad(0, 5f,10f,0);
-        tableLeft.add(nickname);
-        tableLeft.row().colspan(3).fillY().pad(0,0,10f,0);
-        tableLeft.add(join).fillY();
-        tableLeft.row().colspan(3).fillY();
-        tableLeft.add(exit).fillY();
+        tableLeft.add(nickname)
+                .prefHeight(nicknameHeight)
+                .prefWidth(buttonWidth)
+                .padBottom(spacing)
+                .padLeft(spacing)
+        .fillX().fillY();
+        tableLeft.row();
+//        tableLeft.add(highScoreList).align(Align.right);
+        tableLeft.row();
+        tableLeft.add(join)
+                .prefWidth(buttonWidth)
+                .prefHeight(buttonHeight)
+                .padBottom(spacing)
+                .padLeft(spacing);
+        tableLeft.row();
+        tableLeft.add(exit)
+                .padBottom(spacing)
+                .prefHeight(buttonHeight)
+                .prefWidth(buttonWidth)
+                .padLeft(spacing);
+
+        tableRight.row();
+        tableRight.add(highScoreList)
+                .prefHeight(sizeHeight*0.7f)
+                .prefWidth(sizeWidth*0.6f)
+                .padLeft(spacing*2f)
+                .padTop(spacing);
 
         group.setWidth(sizeWidth);
         group.setHeight(sizeHeight);
         group.addActor(tableLeft);
+        group.addActor(tableRight);
 
         stage.addActor(group);
         Gdx.input.setInputProcessor(stage);
-
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor( 0, 0, 0, 0 );
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        backgroundTexture = new Texture(Gdx.files.internal("background.png"));
+        stage.getBatch().begin();
+        stage.getBatch().draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        stage.getBatch().end();
         stage.act();
         stage.draw();
     }
@@ -124,5 +165,18 @@ public class LobbyView extends ApplicationAdapter implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    private Array getFakeHighScoreList(){
+        Array<String> highScores = new Array<>();
+        highScores.add("Gard", "1 million fucking points");
+        highScores.add("Lars", "500000");
+        highScores.add("Tuva", "18");
+        highScores.add("Alexander", "15");
+        highScores.add("Tuva", "18");
+        highScores.add("Alexander", "15");
+        highScores.add("Anders", "0");
+        highScores.add("Anders", "0");
+        return highScores;
     }
 }
